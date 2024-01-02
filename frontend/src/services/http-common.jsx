e1e57c8f7d1355e403081aa999f43ba5870e8ab2
Kadir YALÇİN <96176339+lkadiryalcinl@users.noreply.github.com>
2024-01-02 18:24:40 +0300

    fetch problem solved

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://mekan-bul-api-kadir-yalcin.vercel.app/api",
+  baseURL: "https://mekanbul-api-kadir-yalcin.vercel.app/api",
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json;charset=UTF-8"
